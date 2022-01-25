8fe5f2c3264dc797d5c4b8da0d6e610622735d05
Ivica Nikolic <ivicanikolic.mail@gmail.com>
2022-01-25 21:24:31 +0100

    prvi kontroler

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+
 
 class UserController extends Controller
 {
-    public function test(){
-        return "ovo je test";
+    public function index(){
+        return User::all();
     }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -16,7 +16,7 @@ use App\Http\Controllers\UserController;
 */
 
 
-Route::get('test', [UserController::class, 'test']);
+Route::get('test', [UserController::class, 'index']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
