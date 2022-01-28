1549299b5bc9e810b31613685409543ac738d38b
Ivica Nikolic <ivicanikolic.mail@gmail.com>
2022-01-28 23:47:41 +0100

    pravljene role kontroler

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -38,6 +38,8 @@ use Laravel\Passport\HasApiTokens;
  * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
  * @mixin \Eloquent
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Laravel\Passport\Client[] $clients
+ * @property-read int|null $clients_count
  */
 class User extends Authenticatable
 {
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\Role\RoleController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
@@ -23,6 +24,8 @@ Route::group(['middleware' => 'auth:api'], function(){
     Route::get('user', [UserController::class, 'user']);
     Route::put('user/info', [UserController::class, 'updateInfo']);
     Route::put('user/password', [UserController::class, 'updatePassword']);
+
+    Route::apiResource('roles', RoleController::class);
     Route::apiResource('users', UserController::class);
 });
 
